23d7e31dc35ea5fb0d70f68e03b2d4ca748402a2
arturmh <52570589+ArturM4@users.noreply.github.com>
2022-04-12 20:27:49 +0200

    add bootstrap

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,20 @@
+import { Container } from "react-bootstrap";
 import { Chessboard } from "react-chessboard";
 
 export function Home() {
   return (
     <>
-      <h1>Escacs</h1>
-      <button>Un jugador</button>
-      <button>Multijugador</button>
-      <button>Canviar estils</button>
-      <Chessboard />
+
+      <Container className='p-4 mt-5 rounded-3 bg-secondary d-flex' >
+        <div>
+          <h1>Escacs</h1>
+          <button>Un jugador</button><br />
+          <button>Multijugador</button><br />
+          <button>Canviar estils</button>
+        </div>
+        <Chessboard />
+
+      </Container>
     </>
   )
 }
