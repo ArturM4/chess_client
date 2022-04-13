148b536c93b94f4defa57406d4644f7b10ca8b07
arturmh <52570589+ArturM4@users.noreply.github.com>
2022-04-13 13:31:23 +0200

    Add styles and improve responsiveness

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,27 @@
-import { Container } from "react-bootstrap";
+import { Button, Col, Container, Row, Stack } from "react-bootstrap";
 import { Chessboard } from "react-chessboard";
 
 export function Home() {
   return (
     <>
 
-      <Container className='p-4 mt-5 rounded-3 bg-secondary d-flex' >
-        <div>
-          <h1>Escacs</h1>
-          <button>Un jugador</button><br />
-          <button>Multijugador</button><br />
-          <button>Canviar estils</button>
-        </div>
-        <Chessboard />
 
+      <Container className='p-4 mt-5 rounded-3 bg-secondary' >
+        <Row>
+          <Col xs={3}>
+            <div>
+              <h1>Escacs</h1>
+              <Stack className="mx-auto">
+                <Button size='lg' className='ms-2 py-3 mb-4'>Un Jugador</Button>
+                <Button size='lg' className='ms-2 py-3 mb-4'>Multijugador</Button>
+                <Button size='lg' className='ms-2 py-3'>Canviar estils</Button>
+              </Stack>
+            </div>
+          </Col>
+          <Col xs={9}>
+            <Chessboard />
+          </Col>
+        </Row>
       </Container>
     </>
   )
