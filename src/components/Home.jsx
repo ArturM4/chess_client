5bec2023ff12f91d88b1752fed875c3da18c0447
arturmh <52570589+ArturM4@users.noreply.github.com>
2022-04-13 17:48:14 +0200

    Improve responsiveness for small screen sizes, add styles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,19 @@
 import { Button, Col, Container, Row, Stack } from "react-bootstrap";
 import { Board } from "./Board/Board";
-
+import '../mainstyles.css'
 export function Home() {
   return (
     <>
-      <Container className='p-4 mt-5 rounded-3 bg-secondary' >
+      <Container className='p-4 mt-5 rounded-3' >
         <Row>
-          <Col xs={3}>
-            <div>
-              <h1>Escacs</h1>
-              <Stack className="mx-auto">
-                <Button size='lg' className='ms-2 py-3 mb-4'>Un Jugador</Button>
-                <Button size='lg' className='ms-2 py-3 mb-4'>Multijugador</Button>
-                <Button size='lg' className='ms-2 py-3'>Canviar estils</Button>
-              </Stack>
-            </div>
+          <Col xs={12} md={3}>
+            <Stack className="mx-auto">
+              <Button size='lg' className='c-386ecf ms-md-2 py-3 mb-2 mb-md-4' variant=''>Un Jugador</Button>
+              <Button size='lg' className='c-386ecf ms-md-2 py-3 mb-2 mb-md-4' variant=''>Multijugador</Button>
+              <Button size='lg' className='c-386ecf ms-md-2 py-3 mb-2 mb-md-4' variant=''>Canviar estils</Button>
+            </Stack>
           </Col>
-          <Col xs={9}>
+          <Col xs={12} md={9}>
             <Board />
           </Col>
         </Row>
