0a4fb756c061a5d92bdea4515fceca5c5b4fc27e
arturmh <52570589+ArturM4@users.noreply.github.com>
2022-05-24 14:15:58 +0200

    timers without sync

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,8 +1,9 @@
 import { Chess } from 'chess.js';
 import React, { useCallback, useEffect, useState } from 'react'
-import { Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import { Chessboard } from 'react-chessboard';
 import { useParams } from 'react-router-dom';
+import { useInterval } from '../hooks/useInterval';
 import { usePromotion } from '../hooks/usePromotion';
 import { useResponsiveBoard } from '../hooks/useResponsiveBoard';
 import socket from '../socket/socket'
@@ -22,8 +23,27 @@ export function Game() {
   const { boardWidth } = useResponsiveBoard()
   const [showBoard, setshowBoard] = useState(false);
 
+  const [whiteLastMoveTime, setWhiteLastMoveTime] = useState(180000);
+  const [blackLastMoveTime, setBlackLastMoveTime] = useState(180000);
+  const [whiteTime, setWhiteTime] = useState(180000);
+  const [blackTime, setBlackTime] = useState(180000);
+  const [lastMoveDate, setLastMoveDate] = useState(new Date());
+
+  const [clockStarted, setClockStarted] = useState(false);
+
   useEffect(() => {
     socket.emit('joinGame', gameId)
+  }, [gameId]);
+
+  useInterval(() => {
+    if (game.turn() === 'w') {
+      setWhiteTime(whiteLastMoveTime - ((new Date()).getTime() - lastMoveDate.getTime()))
+    } else {
+      setBlackTime(blackLastMoveTime - ((new Date()).getTime() - lastMoveDate.getTime()))
+    }
+  }, clockStarted ? 100 : null);
+
+  useEffect(() => {
 
     socket.on("gameInit", (isWhite) => {
       setIsPlayerWhite(isWhite)
@@ -33,14 +53,20 @@ export function Game() {
     return () => {
       socket.off('gameInit')
     }
-  }, [gameId]);
+  }, [isPlayerWhite]);
+
 
   const doMove = useCallback((from, to, promotion) => {
     const gameCopy = { ...game };
     const move = gameCopy.move({ from, to, promotion });
-    if (move)
+    if (move) {
       setGame(gameCopy);
-
+      setBlackLastMoveTime(blackTime)
+      setWhiteLastMoveTime(whiteTime)
+      setLastMoveDate(new Date())
+      if (gameCopy.history().length === 2)
+        setClockStarted(true)
+    }
     if (gameCopy.in_check())
       setKingInCheckSquare({
         [getPieceFromPosition(game, { type: 'k', color: game.turn() })]: {
@@ -68,7 +94,7 @@ export function Game() {
     }
     return move;
 
-  }, [game, isPlayerWhite])
+  }, [game, isPlayerWhite, whiteTime, blackTime])
 
   useEffect(() => {
     socket.on("moveDone", ({ from, to, promotion }) => {
@@ -89,6 +115,12 @@ export function Game() {
     return move;
   }
 
+  function handlePromotion(p) {
+    const move = promote(p)
+    if (move)
+      socket.emit('doMove', gameId, { from: move.from, to: move.to, promotion: move.promotion })
+  }
+
   function isDraggablePiece({ piece }) {
     if (isPieceWhite(piece) === isPlayerWhite)
       return true
@@ -101,30 +133,51 @@ export function Game() {
     return 'black'
   }
 
-  function handlePromotion(p) {
-    const move = promote(p)
-    if (move)
-      socket.emit('doMove', gameId, { from: move.from, to: move.to, promotion: move.promotion })
+  function timeFormated(orientation) {
+    let time = 0
+    orientation === 'w' ? time = whiteTime : time = blackTime
+
+    return Math.floor(time / (1000 * 60)).toString().padStart(2, '0') + ":"
+      + (Math.floor(time / 1000) % 60).toString().padStart(2, '0') + "."
+      + Math.floor(time % 1000 / 100)
   }
+
   return (
     <Container className='mt-5'>
-      <div className='boardWrapper'>
-        {showBoard && <>
-          <GameResult showResult={showResult} setShowResult={setShowResult} />
-          <Promotion isPromoting={isPromoting} getPromotionRow={getPromotionRow} turn={game.turn()} handlePromotion={handlePromotion} orientation={boardOrientation().charAt(0)} />
-          <Chessboard
-            boardWidth={boardWidth}
-            position={game.fen()}
-            onPieceDrop={onPieceDrop}
-            isDraggablePiece={isDraggablePiece}
-            boardOrientation={boardOrientation()}
-            onSquareClick={() => cancelPromotion()}
-            onPieceDragBegin={() => cancelPromotion()}
-            customSquareStyles={{ ...kingInCheckSquare }}
-            arePiecesDraggable={arePiecesDraggable}
-          />
-        </>}
-      </div>
+      <Row>
+        <Col md={0} lg={2}>
+        </Col>
+        <Col xs={12} md={10} lg={8}>
+          <div className='boardWrapper'>
+            {showBoard && <>
+              <GameResult showResult={showResult} setShowResult={setShowResult} />
+              <Promotion isPromoting={isPromoting} getPromotionRow={getPromotionRow} turn={game.turn()} handlePromotion={handlePromotion} orientation={boardOrientation().charAt(0)} />
+              <Chessboard
+                boardWidth={boardWidth}
+                position={game.fen()}
+                onPieceDrop={onPieceDrop}
+                isDraggablePiece={isDraggablePiece}
+                boardOrientation={boardOrientation()}
+                onSquareClick={() => cancelPromotion()}
+                onPieceDragBegin={() => cancelPromotion()}
+                customSquareStyles={{ ...kingInCheckSquare }}
+                arePiecesDraggable={arePiecesDraggable}
+              />
+            </>}
+          </div>
+        </Col>
+        <Col xs={12} md={2} lg={2}>
+          {boardOrientation() === 'white' && <>
+            <p className='fs-1 text-white'>{timeFormated('b')}</p>
+            <p className='fs-1 text-white'>{timeFormated('w')}</p>
+          </>}
+          {boardOrientation() === 'black' && <>
+            <p className='fs-1 text-white'>{timeFormated('w')}</p>
+            <p className='fs-1 text-white'>{timeFormated('b')}</p>
+          </>}
+        </Col>
+      </Row>
+
     </Container >
   )
 }
